be48cbf8deec4522695bbb07ca29f5913220b0c9
MltsKsenia <mltsksenia@gmail.com>
2024-11-02 22:35:52 +0200

    returning services

---
+++ Frontend/src/services/app.ts
@@ -0,0 +1,19 @@
+// servises/app.ts
+import axios from 'axios';
+const API_URL = '/api';
+export const fetchItems = async () => {
+    try {
+        const response = await axios.get(`${API_URL}/items`);
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching items:', error);
+    }
+};
+export const addItem = async (itemData: FormData) => {
+    try {
+        const response = await axios.post(`${API_URL}/items`, itemData);
+        return response.data;
+    } catch (error) {
+        console.error('Error adding item:', error);
+    }
+};
