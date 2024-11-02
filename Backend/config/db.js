a40934b9f87fe05fc7d81ed4835e04e1b6efe07f
MltsKsenia <mltsksenia@gmail.com>
2024-11-02 19:01:24 +0200

    connecting data

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -6,9 +6,9 @@ const db = knex(knexConfig.development);
 
 // Log database connection status
 db.raw('SELECT 1')
-    .then(() => console.log(`Database connection successful in ${environment} mode`))
+    .then(() => console.log(`Database connection successful in ${process.env.DATABASE_URL} mode`))
     .catch(err => {
-        console.error(`Database connection failed in ${environment} mode:`, err);
+        console.error(`Database connection failed in ${process.env.DATABASE_URL} mode:`, err);
         process.exit(1); // Exit the process if the connection fails
     });
 
