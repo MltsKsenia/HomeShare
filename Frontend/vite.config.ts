e856b823c78f0eb2c422ff1a912021a6c05d8139
MltsKsenia <mltsksenia@gmail.com>
2024-11-02 19:23:06 +0200

    connecting data

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,8 +1,21 @@
 // vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  server: {
+    proxy: {
+
+      '/api': {
+        target: process.env.VITE_BACKEND_URL || 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
