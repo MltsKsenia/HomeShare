922602538fb9af5d66a7a6c1a2e518681a8e63cc
MltsKsenia <mltsksenia@gmail.com>
2024-11-02 16:29:59 +0200

    trying to fix backend problem

--- Backend/app.js
+++ Backend/app.js
@@ -26,11 +26,14 @@ app.use('/api', itemRoutes);
 app.use('/api', reservationRoutes);
 
 // Serve static files from the React app
-app.use(express.static(path.resolve('/opt/render/project/src/Frontend/dist')));
+console.log('Serving static files from:', path.resolve(__dirname, '..', 'Frontend', 'dist'));
+app.use(express.static(path.resolve(__dirname, '..', 'Frontend', 'dist')));
 
 // This handles any requests that don't match the ones above, returning the React app
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve('/opt/render/project/src/Frontend/dist/index.html'));
+    console.log('Request for:', req.originalUrl);
+    console.log('Sending file:', path.resolve(__dirname, '..', 'Frontend', 'dist', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '..', 'Frontend', 'dist', 'index.html'));
 });
 
 module.exports = app;
